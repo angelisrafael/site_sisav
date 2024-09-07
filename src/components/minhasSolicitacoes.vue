bfb2f3dbc3faf0f5da06c254d17e944ad8a6e1de
kepler4567 <kepler.batista456@gmail.com>
2024-09-07 14:49:05 -0300

    finalizado tela de solicitacoes

--- src/components/minhasSolicitacoes.vue
+++ src/components/minhasSolicitacoes.vue
@@ -1,76 +1,100 @@
 <template>
-    <div class="container">
-        <div class="buttons">
-            <button @click="currentView = 'notasEFaltas'">Novas</button>
-            <button @click="currentView = 'AAC'">Minhas Solicitações</button>
-            <button @click="currentView = 'historico'">Programas e Disciplinas</button>
-            <button @click="currentView = 'comprovante'">Disciplinas Ofertadas</button>
-        </div>
-
-        <div class="table-container">
-            <table class="grade-table">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Nome</th>
-                        <th>Data de criação</th>
-                        <th>Solicitado por</th>
-                        <th>Alterado Por</th>
-                        <th>Status</th>
-                        <th>Ultima alteração</th>
-
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>123.456</td>
-                        <td>Emissão de histórico escolar</td>
-                        <td>04/11/2023</td>
-                        <td>ra11865</td>
-                        <td>ra169581</td>
-                        <td>Concluído</td>
-                        <td>04/11/2023</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+  <div class="container">
+    <div class="buttons">
+      <button :class="{ active: currentView === 'novas' }" @click="currentView = 'novas'">Novas</button>
+        <button :class="{ active: currentView === 'solicitacoes' }" @click="currentView = 'solicitacoes'">Minhas Solicitações</button>
     </div>
+
+    <div class="table-container">
+      <table class="grade-table">
+        <div v-if="currentView === 'solicitacoes'" class="table-container" id="historico">
+          <table class="grade-table">
+            <thead>
+              <tr>
+                <th>Cód da disciplina.</th>
+                <th>Nome do documento</th>
+                <th>Data</th>
+                <th>Solicitado por</th>
+                <th>Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="solicitacoes in solicitacoes" :key="solicitacoes.id">
+                <td>{{ solicitacoes.id }}</td>
+                <td>{{ solicitacoes.nome_documento }}</td>
+                <td>{{ solicitacoes.data_criacao }}</td>
+                <td>{{ solicitacoes.solicitado_por }}</td>
+                <td>{{ solicitacoes.status }}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </table>
+    </div>
+  </div>
 </template>
- 
+
 <script>
+import axios from 'axios';
 export default {
-    name: 'minhasSolicitacoes',
-    props: {
-        msg: String
+  data() {
+    return {
+      userRA: localStorage.getItem('userRA'),
+      currentView: '',
+      solicitacoes: [],
+    };
+  },
+  watch: {
+    currentView(newValue) {
+      if (newValue === 'solicitacoes') {
+        this.fetchNotasEFaltasData();
+      }
     }
+  },
+  methods: {
+    async fetchNotasEFaltasData() {
+      try {
+        const response = await axios.get('http://localhost:3000/solicitacoes/' + this.userRA);
+        this.solicitacoes = response.data.data;
+      } catch (error) {
+        console.error('Erro ao buscar dados de Notas e Faltas:', error);
+      }
+    },
+
+  },
+
+  name: 'minhasSolicitacoes',
+  props: {
+    msg: String
+  }
 }
 </script>
- 
- <!-- Add "scoped" attribute to limit CSS to this component only -->
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .buttons {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 10vh;
-    background-color: #ffffff;
-    padding: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 10vh;
+  background-color: #ffffff;
+  padding: 30px;
 }
 
 button {
-    margin: 0 10px;
-    padding: 15px 30px;
-    border: none;
-    background-color: #000000;
-    color: white;
-    cursor: pointer;
-    font-size: 16px;
-    border-radius: 5px;
-    transition: background-color 0.3s ease;
+  margin: 0 10px;
+  padding: 15px 30px;
+  border: none;
+  background-color: #000000;
+  color: white;
+  cursor: pointer;
+  font-size: 16px;
+  border-radius: 5px;
+  transition: background-color 0.3s ease;
 }
 
 button:hover {
-    background-color: #ea7474;
+  background-color: #ea7474;
 }
 
 .table-container {
@@ -94,7 +118,8 @@ button:hover {
   text-align: left;
 }
 
-.grade-table th, .grade-table td {
+.grade-table th,
+.grade-table td {
   padding: 12px 15px;
   border: 1px solid #ddd;
 }
@@ -115,4 +140,3 @@ button:hover {
   margin-bottom: 5px;
 }
 </style>
- 
